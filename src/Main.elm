c1ef0086ee125d51567f50066ea31be05a4d76f4
Vit Brunner <vit.brunner@gmail.com>
2023-01-30 14:49:00 +0100

    Separate core go to own module

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Browser
 import Browser.Dom as BD
 import Browser.Events
+import Go exposing (..)
 import Html as H
 import Html.Attributes as HA
 import Json.Decode as D
@@ -11,21 +12,8 @@ import Svg.Attributes as SA
 import Task
 
 
-coordRange =
-    1000
-
-
 boardSize =
     13
-
-
-stoneR =
-    -- 9x9: 55.6, 13x13: 38.5, 19x19: 26.3
-    38
-
-
-connectedDistance =
-    1.4142
 
 
 main =
@@ -50,27 +38,6 @@ type alias Model =
     }
 
 
-type alias Stones =
-    List Stone
-
-
-type alias Links =
-    List ( Stone, Stone )
-
-
-type alias Stone =
-    ( Player, Coords )
-
-
-type Player
-    = Black
-    | White
-
-
-type alias Coords =
-    { x : Int, y : Int }
-
-
 init : () -> ( Model, Cmd msg )
 init _ =
     ( { stones = []
@@ -81,61 +48,6 @@ init _ =
       }
     , Cmd.none
     )
-
-
-otherPlayer : Player -> Player
-otherPlayer p =
-    case p of
-        White ->
-            Black
-
-        Black ->
-            White
-
-
-withinBoard : Stone -> Bool
-withinBoard ( _, coords ) =
-    let
-        isWithin : Int -> Bool
-        isWithin coord =
-            coord > stoneR && coord < coordRange - stoneR
-    in
-    isWithin coords.x && isWithin coords.y
-
-
-distance : Stone -> Stone -> Float
-distance ( _, c1 ) ( _, c2 ) =
-    sqrt <| toFloat ((c1.x - c2.x) ^ 2 + (c1.y - c2.y) ^ 2)
-
-
-overlaps : Stone -> Stones -> Bool
-overlaps stone =
-    List.any (\s -> distance stone s < 2 * stoneR)
-
-
-newLinks : Stone -> Stones -> Links
-newLinks stone =
-    let
-        ( player, _ ) =
-            stone
-
-        maybeLink s1 s2 =
-            if distance s1 s2 < 2 * stoneR * connectedDistance then
-                Just ( s1, s2 )
-
-            else
-                Nothing
-    in
-    List.filter (\( p, _ ) -> p == player) >> List.filterMap (maybeLink stone)
-
-
-playIfLegal : Stone -> Stones -> Maybe Stones
-playIfLegal stone stones =
-    if withinBoard stone && (not <| overlaps stone stones) then
-        Just <| stone :: stones
-
-    else
-        Nothing
 
 
 
