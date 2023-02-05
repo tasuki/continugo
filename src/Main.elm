2018df675608426289d280ed30659e32fd12fdab
Vit Brunner <vit.brunner@gmail.com>
2023-02-05 10:51:15 +0100

    Show lines like on a go board

--- src/Main.elm
+++ src/Main.elm
@@ -22,6 +22,10 @@ boardSize =
     13
 
 
+preciseR =
+    coordRange / (boardSize * 2)
+
+
 main =
     Browser.document
         { init = init
@@ -200,6 +204,73 @@ subscriptions model =
 -- VIEW
 
 
+offset : Float -> Float
+offset lines =
+    lines * preciseR * 2 - preciseR
+
+
+viewLines : List (Svg msg)
+viewLines =
+    let
+        line x1 y1 x2 y2 =
+            Svg.line
+                [ SA.x1 <| String.fromFloat x1
+                , SA.y1 <| String.fromFloat y1
+                , SA.x2 <| String.fromFloat x2
+                , SA.y2 <| String.fromFloat y2
+                , SA.stroke "#993"
+                , SA.strokeWidth "1.5"
+                ]
+                []
+
+        ( start, end ) =
+            ( preciseR, offset boardSize )
+
+        offsets =
+            List.range 1 boardSize |> List.map toFloat
+
+        horizontal =
+            List.map (\o -> line start (offset o) end (offset o)) offsets
+
+        vertical =
+            List.map (\o -> line (offset o) start (offset o) end) offsets
+    in
+    horizontal ++ vertical
+
+
+viewStars : List (Svg msg)
+viewStars =
+    let
+        ( a, z ) =
+            ( 4, boardSize - 3 )
+
+        star x y =
+            Svg.circle
+                [ SA.cx <| String.fromFloat (offset x)
+                , SA.cy <| String.fromFloat (offset y)
+                , SA.r <| String.fromInt 4
+                , SA.fill "#993"
+                ]
+                []
+    in
+    [ star a a
+    , star z a
+    , star a z
+    , star z z
+    ]
+
+
+hideLines : Stone -> Svg Msg
+hideLines { spot } =
+    Svg.circle
+        [ SA.cx <| String.fromInt spot.x
+        , SA.cy <| String.fromInt spot.y
+        , SA.r <| String.fromInt <| stoneR * 6
+        , SA.fill "#EEA"
+        ]
+        []
+
+
 viewStone : Stone -> Svg Msg
 viewStone { player, spot } =
     let
@@ -246,7 +317,7 @@ viewLiberty spot =
         [ SA.cx <| String.fromInt spot.x
         , SA.cy <| String.fromInt spot.y
         , SA.r <| String.fromInt stoneR
-        , SA.stroke "#BB9"
+        , SA.stroke "#BB6"
         , SA.strokeWidth "5"
         , SA.fill "transparent"
         ]
@@ -280,6 +351,13 @@ spotKeyStr s =
     "-" ++ String.fromInt s.x ++ "-" ++ String.fromInt s.y
 
 
+hideLinesKeyed : Stone -> ( String, Svg Msg )
+hideLinesKeyed stone =
+    ( "hide" ++ spotKeyStr stone.spot
+    , lazy hideLines stone
+    )
+
+
 viewKeyedStone : Stone -> ( String, Svg Msg )
 viewKeyedStone stone =
     ( "stone" ++ spotKeyStr stone.spot
@@ -309,7 +387,15 @@ view model =
         [ H.div [ HA.id "board" ]
             [ Svg.svg
                 [ SA.viewBox <| intsToStr [ 0, 0, coordRange, coordRange ] ]
-                [ lazy3 Svg.node "g" [ SA.id "ghostLinks" ] <|
+                [ Svg.defs []
+                    [ Svg.filter [ SA.id "blur-filter" ]
+                        [ Svg.feGaussianBlur [ SA.in_ "SourceGraphic", SA.stdDeviation "50" ] [] ]
+                    ]
+                , lazy3 Svg.node "g" [ SA.id "lines" ] <| viewLines
+                , lazy3 Svg.node "g" [ SA.id "viewStars" ] <| viewStars
+                , lazy3 Svg.Keyed.node "g" [ SA.id "hideLines", SA.filter "url(#blur-filter)" ] <|
+                    List.map hideLinesKeyed (stoneList model.stones)
+                , lazy3 Svg.node "g" [ SA.id "ghostLinks" ] <|
                     List.map viewGhostLink (Maybe.map getStoneLinks model.ghostStone |> Maybe.withDefault [])
                 , lazy3 Svg.node "g" [ SA.id "ghostStone" ] <|
                     List.filterMap identity [ Maybe.map viewGhostStone model.ghostStone ]
